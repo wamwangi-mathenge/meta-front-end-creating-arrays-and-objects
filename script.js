c6b81bb49629a029dbaf176b53fec9c8a3d775e3
Brian <wamwangimathenge@gmail.com>
2023-07-26 13:40:38 +0300

    Complete the tasks

---
+++ script.js
@@ -0,0 +1,31 @@
+// Create a new empty array literal and assign it to the variable cars
+var cars = [];
+
+// Add 5 your 5 favorite car brands as strings using the push() method
+cars.push("Audi", "Lexus", "Mercedes", "Porsche", "Toyota")
+// console.log(cars)
+
+// Remove the 5th car brand from the array using the pop() method
+cars.pop()
+// console.log(cars)
+
+// Add a new car brand using the push() method
+cars.push("Mazda")
+// console.log(cars)
+
+// Use console.log to show the 3rd item from the cars array in the console
+console.log(cars[2])
+
+// Create a new empty object literal and assign it to the variable favCar
+var favCar = {};
+
+// Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+favCar.color = "Black";
+console.log(favCar);
+
+// Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+favCar.convertible = false;
+console.log(favCar);
+
+// Use console log to log the entire favCar object.
+console.log(favCar);
